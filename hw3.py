5250ac2edf2b13708ebc430f13e0722b50a7ca5d
songmc <127750357+songminchang@users.noreply.github.com>
2023-03-26 23:54:50 +0900

    Create hw3.py

---
+++ hw3.py
@@ -0,0 +1,18 @@
+def get_fixed_price(price):
+  global price
+
+정가1 = 100*b/(a+100)
+정가2 = 100*c/(a+100)
+  
+
+
+import price
+
+a = int(input("할인율은"?))
+price.get_fixed_price(price)
+
+b = int(input("A 상품의 할인된 가격은?"))
+c = int(input("B 상품의 할인된 가격은? "))
+
+print("A 상품의 정가는 ,정가1, 원")
+print("B 상품의 정가는 ,정가2, 원")

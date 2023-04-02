deebcfec0c64bbeb105f9a34a120baf9625f83d7
songmc <127750357+songminchang@users.noreply.github.com>
2023-04-03 00:16:53 +0900

    Create hw4.py

---
+++ hw4.py
@@ -0,0 +1,22 @@
+def rep_char(a):
+  reala = a
+  return f'{realc}'
+
+a = input("Input his/her name:")
+
+
+
+rmsg = rep_char(a)
+print(msg)
+
+
+
+def welcome_message(msg):
+  nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+  
+  
+  rep_char('-',nstr*2 + 4)
+  print(f'  {msg}  ')
+  rep_char('-',nstr*2 + 4)
+  
+welcome_message('Hello' a '\n Welcome to Seoul.')
